9daa950f475952e4d348dd97413e720a00d25fba
Plato <otalpster@gmail.com>
2019-08-21 20:49:29 +0300

    config.h: change font to Iosevka Custom Regular

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mononoki Nerd Font Mono:pixelsize=14:antialias=true:hinting=true";
+static char *font = "Iosevka Custom:style=Regular:pixelsize=14:antialias=true:hinting=true";
 static int borderpx = 6;
 
 /*

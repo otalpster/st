cf90c714588bdbccadbbf7f882a7bb0f9e7ef32a
Plato <otalpster@gmail.com>
2019-09-23 15:00:14 +0300

    config.h: change scroll speed to two lines

--- config.h
+++ config.h
@@ -165,8 +165,8 @@ static MouseShortcut mshortcuts[] = {
 
 MouseKey mkeys[] = {
 	/* button               mask            function        argument */
-	{ Button4,              XK_NO_MOD,      kscrollup,      {.i =  1} },
-	{ Button5,              XK_NO_MOD,      kscrolldown,    {.i =  1} },
+	{ Button4,              XK_NO_MOD,      kscrollup,      {.i =  2} },
+	{ Button5,              XK_NO_MOD,      kscrolldown,    {.i =  2} },
 };
 
 /* Internal keyboard shortcuts. */
